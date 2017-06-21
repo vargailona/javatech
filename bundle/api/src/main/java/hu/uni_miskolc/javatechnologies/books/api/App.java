c18c0abe4e54022eef89a7cdebfec3bfdc3c3d8e
Ilona <Ilona@VargaIlona-PC>
2017-06-21 21:30:00 +0200

    Books project api and service moduls have been created.

---
+++ bundle/api/src/main/java/hu/uni_miskolc/javatechnologies/books/api/App.java
@@ -0,0 +1,13 @@
+package hu.uni_miskolc.javatechnologies.books.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
